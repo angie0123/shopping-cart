c28411644a3a237830842536ff083c32d842d078
Angela Sun <angelasun1995@gmail.com>
2022-05-06 15:49:09 -0400

    Add rendering functionality to store items in shop page

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,5 +1,45 @@
 const Shop = () => {
-  return <h1>Hello from Shop!</h1>;
+  const storeItems = [
+    {
+      img: '/images/aloe.jpg',
+      name: 'Aloe',
+      price: '$34.99',
+    },
+    {
+      img: '/images/cactus.jpg',
+      name: 'Cactus',
+      price: '$29.99',
+    },
+    {
+      img: '/images/leafy.jpg',
+      name: 'Lauri Fern',
+      price: '$24.99',
+    },
+  ];
+
+  const list = storeItems.map((item, index) => {
+    return (
+      <li key={index}>
+        <div>
+          <img
+            style={{ width: '300px', height: '300px' }}
+            src={window.location.origin + item.img}
+            alt={item.name}
+          />
+          <div>
+            {item.name} {item.price}
+          </div>
+        </div>
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Shop our items</h1>
+      <ul>{list}</ul>
+    </div>
+  );
 };
 
 export default Shop;
