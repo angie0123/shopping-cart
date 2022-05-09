e5e46cbb5a1e4ba07786f8a663326fe4ddd0a9e9
Angela Sun <angelasun1995@gmail.com>
2022-05-08 22:43:25 -0400

    Add message for empty cart

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,14 +1,28 @@
+import { Link } from 'react-router-dom';
 import CartItem from './CartItem';
 
 const CartItems = ({ cartItems, inputChangeHandler }) => {
-  const list = cartItems.map((item) => {
-    return (
-      <li key={item.id}>
-        <CartItem item={item} inputChangeHandler={inputChangeHandler} />
-      </li>
-    );
-  });
-  return <ul>{list}</ul>;
+  const list =
+    cartItems.length > 0
+      ? cartItems.map((item) => {
+          return (
+            <li key={item.id}>
+              <CartItem item={item} inputChangeHandler={inputChangeHandler} />
+            </li>
+          );
+        })
+      : null;
+  return (
+    <>
+      {list ? (
+        <ul>{list}</ul>
+      ) : (
+        <div>
+          Your cart is empty <Link to="/shop">Continue shopping</Link>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default CartItems;
