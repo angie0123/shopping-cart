eacb810b669f702d210434cf964f8647fda1a84b
Angela Sun <angelasun1995@gmail.com>
2022-05-09 00:21:43 -0400

    Styled home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <h1>Hello from Home!</h1>;
+  return (
+    <div className="main-content">
+      <img
+        className="background"
+        src={window.location.origin + '/images/background.jpg'}
+        alt="flower"
+      />
+      <div className="announcement">Spring arrivals are here</div>
+    </div>
+  );
 };
 
 export default Home;
