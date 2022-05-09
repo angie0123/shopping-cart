6922eea7a7e18bedd6563a8431aefe5a05500a04
Angela Sun <angelasun1995@gmail.com>
2022-05-08 21:41:39 -0400

    Add test for updating cart info when item is added

--- src/App.test.js
+++ src/App.test.js
@@ -1,63 +1,26 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import Shop from './Shop';
+import App from './App';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-const shopItems = [
-  {
-    img: '/images/aloe.jpg',
-    name: 'Aloe',
-    price: '$34.99',
-    id: 0,
-  },
-  {
-    img: '/images/cactus.jpg',
-    name: 'Cactus',
-    price: '$29.99',
-    id: 1,
-  },
-  {
-    img: '/images/leafy.jpg',
-    name: 'Lauri Fern',
-    price: '$24.99',
-    id: 2,
-  },
-];
-
-jest.mock('../components/ShopItems', () => ({ items, handleAddToCart }) => (
-  <div>
-    <ul data-testid="itemsDisplayed">
-      {shopItems.map((item) => {
-        return (
-          <li key={item.id}>
-            <div
-              onClick={() => handleAddToCart(item.id)}
-              data-testid="addToCart"
-            >
-              {' '}
-              Add To Cart
-            </div>
-          </li>
-        );
-      })}
-    </ul>
-  </div>
-));
-
-describe('shop', () => {
-  it('updates cart items when user adds an item to cart', () => {
-    render(<Shop />);
-    const addBtn = screen.getAllByTestId('addToCart')[0];
+describe('app', () => {
+  it('updates cart items in Nav when user adds an item to cart in Shop', () => {
+    render(<App />);
+    userEvent.click(screen.getByRole('link', { name: 'Shop' }));
+    const addBtn = screen.getAllByText(/add to cart/i)[0];
     userEvent.click(addBtn);
-    expect(screen.getByText('Cart: 1')).toBeInTheDocument();
+    const cartLink = screen.getByRole('link', { name: 'Cart: 1' });
+    expect(cartLink).toBeInTheDocument();
   });
-  it('updates cart items when user adds multiple items to cart', () => {
-    render(<Shop />);
-    const addBtn1 = screen.getAllByTestId('addToCart')[0];
-    const addBtn2 = screen.getAllByTestId('addToCart')[1];
+  it('updates cart items in Nav when user adds multiple items to cart', () => {
+    render(<App />);
+    userEvent.click(screen.getByRole('link', { name: 'Shop' }));
+    const addBtn1 = screen.getAllByText(/add to cart/i)[0];
+    const addBtn2 = screen.getAllByText(/add to cart/i)[1];
     userEvent.click(addBtn1);
     userEvent.click(addBtn2);
-    expect(screen.getByText('Cart: 2')).toBeInTheDocument();
+    const cartLink = screen.getByRole('link', { name: 'Cart: 2' });
+    expect(cartLink).toBeInTheDocument();
   });
 });
